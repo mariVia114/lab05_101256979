a6afb9eda0365f104edd2e4d4d464f9dbc17567c
Marie <marievianca.pagaduan@georgebrown.ca>
2023-03-12 19:04:44 -0400

    exercise one done

---
+++ exercise-1/hello-world.ts
@@ -0,0 +1,11 @@
+// var greeter = function (name) {
+//     console.log('Hello', name);
+// }
+
+// greeter('John Smith')
+
+var greeter = (firstName, lastName) => {
+    console.log(`Hello ${firstName} ${lastName}`);
+}
+
+greeter("John", "Smith")
